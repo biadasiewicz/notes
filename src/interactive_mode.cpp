2027ff2e016df801f1f5e2b385cbe1264a476c58
tomek <biadasiewicz@protonmail.com>
2017-10-28 12:05:51 +0200

    fix prompt bug if minutes is less than 10

--- src/interactive_mode.cpp
+++ src/interactive_mode.cpp
@@ -36,7 +36,10 @@ decltype(auto) prompt(std::string& line)
 {
 	time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 	std::tm* time = localtime(&t);
-	cout << time->tm_hour << ":" << time->tm_min << " >>> ";
+	cout << time->tm_hour << ":";
+	if(time->tm_min < 10)
+		cout << "0";
+	cout << time->tm_min << " >>> ";
 	return std::getline(cin, line);
 }
 
